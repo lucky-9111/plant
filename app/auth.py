d56cef5134b89bec07b433217a26b439bccbfeb7
lucky-9111 <lucky9111sharma@gmail.com>
2026-08-08 17:11:34 +0530

    revert

--- app/auth.py
+++ app/auth.py
@@ -4,7 +4,7 @@ from fastapi.responses import RedirectResponse
 
 
 def hash_password(password: str) -> str:
-    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8"),
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
 
 def verify_password(password: str, hashed: str) -> bool:
